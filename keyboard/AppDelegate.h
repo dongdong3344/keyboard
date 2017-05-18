dcd8f69aff0544d39988b3ec6fec4dc31bbcbb1d
林冬冬 <dongdong3344@163.com>
2017-05-18 13:52:47 +0800

    Initial Commit

---
+++ keyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  keyboard
+//
+//  Created by Ringo on 2017/5/18.
+//  Copyright © 2017年 Ringo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ keyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  keyboard
+//
+//  Created by Ringo on 2017/5/18.
+//  Copyright © 2017年 Ringo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
